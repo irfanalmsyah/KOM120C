d1680fb2783b2041660c9e9fcd9fa2cc9b1ba25b
Irfan Alamsyah <88070126+irfanalmsyah@users.noreply.github.com>
2023-02-13 19:43:45 +0700

    pertemuan 3

--- solver/KOM120C-03-A-Data.cpp
+++ solver/KOM120C-03-A-Data.cpp
@@ -11,6 +11,7 @@ class DaftarKode {
     private:
         map<string,Barang> dt; // daftar kode barang
         int size; // ukuran daftar kode barang yang ada
+        
     public:
         DaftarKode() { dt.clear(); size = 0; }
         void init() { // membaca daftar kode barang dari stdin
@@ -20,7 +21,8 @@ class DaftarKode {
             for (int i = 0; i < n; i++) {
                 Barang b;
                 cin >> b.kode >> b.nama >> b.harga;
-                dt[b.kode] = b;            }
+                dt[b.kode] = b;
+            }
         } 
         string getNama(string kode) { // mendapatkan nama barang dari kode
             return dt[kode].nama;
@@ -28,42 +30,60 @@ class DaftarKode {
         int getHarga(string kode) { // mendapatkan harga barang dari kode
             return dt[kode].harga;
         }
+        bool isExist(string kode) { // mengecek apakah kode barang ada
+            return dt.find(kode) != dt.end();
+        }
 };
 
 class Penjualan {
     private:
         DaftarKode daftar; // daftar kode barang yang ada
-        map<string,int> st; // data penjualan
+        map<string,int> dt; // data penjualan
         int size; // ukuran data penjualan
     public:
     Penjualan() {
         daftar.init();
-        size=0;
+        size=0; 
     }
     void add(string kode, int jumlah) { // menambah data penjualan
-        st[kode] += jumlah;
-        size++;
+        if (daftar.isExist(kode)) { 
+            dt[kode] += jumlah;
+            size++;
+        }
     }
     int getJumlah(string kode) { // mendapatkan jumlah barang yang terjual
-        return st[kode];
+        return dt[kode];
     }
     int getTotalHarga() { // mendapatkan total harga penjualan
         int total = 0;
-        for (auto it = st.begin(); it != st.end(); it++) {
+        for (auto it = dt.begin(); it != dt.end(); it++) {
             total += daftar.getHarga(it->first) * it->second;
         }
         return total;
     }
     void print() { // mencetak data penjualan
-        for (auto it = st.begin(); it != st.end(); it++) {
-            cout << daftar.getNama(it->first) << " " << it->second << endl;
+        for (auto it = dt.begin(); it != dt.end(); it++) {
+            cout << it->first << " " << daftar.getNama(it->first) << " " << it->second << " " << fixed << setprecision(2) << (double)daftar.getHarga(it->first) * it->second / 1000000 << endl;
         }
+        cout << "Total Nilai : " << fixed << setprecision(2) << (double)getTotalHarga() / 1000000 << " juta rupiah" << endl;
+    }
+    void printSortedByTotalDividedByHarga() { // mencetak data penjualan yang diurutkan berdasarkan total harga / harga barang
+        vector<pair<string,double>> v;
+        for (auto it = dt.begin(); it != dt.end(); it++) {
+            v.push_back({it->first, (double)daftar.getHarga(it->first) * it->second / getTotalHarga()});
+        }
+        sort(v.begin(), v.end(), [](pair<string,double> a, pair<string,double> b) {
+            return a.second > b.second;
+        });
+        for (auto it = v.begin(); it != v.end(); it++) {
+            cout << it->first << " " << daftar.getNama(it->first) << " " << dt[it->first] << " " << fixed << setprecision(2) << (double)daftar.getHarga(it->first) * dt[it->first] / 1000000 << endl;
+        }
+        cout << "Total Nilai : " << fixed << setprecision(2) << (double)getTotalHarga() / 1000000 << " juta rupiah" << endl;
     }
 };
 
 int main() {
     DaftarKode daftar;
-    daftar.init();
     Penjualan penjualan;
     int n;
     cin >> n;
@@ -73,5 +93,6 @@ int main() {
         cin >> kode >> jumlah;
         penjualan.add(kode, jumlah);
     }
-
+    penjualan.printSortedByTotalDividedByHarga();
+    return 0;
 }
